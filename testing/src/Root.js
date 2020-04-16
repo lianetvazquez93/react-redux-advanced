8aeabe59177a65915dd798cc75d3782a0132bb21
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-04-16 17:20:07 +0200

    * Middleware creation  * Emitting warnings

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import async from "middlewares/async";
+import stateValidator from "middlewares/stateValidator";
 import reducers from "reducers";
 
 export default ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, initialState, applyMiddleware(async))}>
+    <Provider store={createStore(reducers, initialState, applyMiddleware(async, stateValidator))}>
       {children}
     </Provider>
   );
