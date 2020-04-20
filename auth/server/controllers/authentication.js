7ca4b3d672fcfeed520cb5d2834109f670f17ea3
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-04-20 17:02:53 +0200

    * Searching for users  * Creating user records

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,3 +1,23 @@
-exports.signup = (req, res, next) => {
-  res.send({ success: true });
+const User = require("../models/user");
+
+exports.signup = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(422).send({ error: "Email is in use" });
+    }
+
+    const newUser = new User({
+      email,
+      password,
+    });
+
+    await newUser.save();
+    res.status(201).send({ success: true });
+  } catch (err) {
+    next(err);
+  }
 };
