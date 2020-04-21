d75ac5ed32371b2be266ddc159cbb851b9d6554f
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-04-21 15:28:18 +0200

    Encrypting passwords with bcrypt

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -3,6 +3,10 @@ const User = require("../models/user");
 exports.signup = async (req, res, next) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(422).send({ error: "You must provide email and password" });
+  }
+
   try {
     const user = await User.findOne({ email });
 
--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   email: {
     type: String,
+    required: true,
     unique: true,
     lowercase: true,
   },
-  password: String,
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", function(next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+
+    user.password = hash;
+    next();
+  });
 });
 
 module.exports = mongoose.model("user", userSchema);
