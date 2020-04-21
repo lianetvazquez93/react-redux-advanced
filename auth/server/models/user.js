463078c12ac2bfb0ad04e13409c9315e4426c50a
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-04-21 17:26:56 +0200

    * Signing in with local strategy  * Bcrypt full circle  * Signing users in

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -27,4 +27,14 @@ userSchema.pre("save", function(next) {
   });
 });
 
+userSchema.methods.comparePasswords = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
+
 module.exports = mongoose.model("user", userSchema);
--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -1,9 +1,35 @@
 const passport = require("passport");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
 const User = require("../models/user");
 const config = require("../config");
 
+const localOptions = { usernameField: "email" };
+
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    user.comparePasswords(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+
+      if (!isMatch) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  });
+});
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader("authorization"),
   secretOrKey: config.secret,
@@ -24,3 +50,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 passport.use(jwtLogin);
+passport.use(localLogin);
