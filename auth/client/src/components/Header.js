e3a7d010c8263fa72dd5e40eb9f4fbac984d1354
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-04-24 15:37:22 +0200

    Header styling

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
+import "./HeaderStyle.css";
+
 class Header extends React.Component {
   renderLinks() {
     if (this.props.authenticated) {
@@ -22,7 +24,7 @@ class Header extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="header">
         <Link to="/">Redux Auth</Link>
         {this.renderLinks()}
       </div>
