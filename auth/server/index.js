849e08ae4afe2288137d33975a5b7ea0e8bf226b
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-04-24 10:57:40 +0200

    Solution to cors errors

--- auth/server/index.js
+++ auth/server/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const router = require("./router");
 
 mongoose.connect("mongodb://localhost/auth_db", {
@@ -14,6 +15,7 @@ mongoose.connect("mongodb://localhost/auth_db", {
 const app = express();
 
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
